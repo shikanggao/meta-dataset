adf6478b761e6e1fccf3c9b2b42eb58325b1c44f
Shikang <chrisgsk5@gmail.com>
2021-11-22 21:32:03 -0800

    Update test script

--- test_run.sh
+++
@@ -1,8 +0,0 @@
-export EXPNAME=baseline_mvrgbd_syn
-export JOBNAME=${EXPNAME}
-python -m meta_dataset.train \
---records_root_dir=$RECORDS \
---train_checkpoint_dir=${EXPROOT}/checkpoints/${JOBNAME} \
---summary_dir=${EXPROOT}/summaries/${JOBNAME} \
---gin_config=meta_dataset/learn/gin/default/${JOBNAME}.gin \
---gin_bindings="Trainer.experiment_name='$EXPNAME'"
---
+++ test_train.sh
@@ -0,0 +1,15 @@
+export SOURCE=mvrgbd_syn
+# export EXPNAME=baseline
+export EXPNAME=maml
+# export EXPNAME=matching
+# export EXPNAME=prototypical
+# export EXPNAME=relationnet
+export JOBNAME=${EXPNAME}_${SOURCE}
+
+python -m meta_dataset.train \
+--records_root_dir=$RECORDS \
+--train_checkpoint_dir=${EXPROOT}/checkpoints/${JOBNAME} \
+--summary_dir=${EXPROOT}/summaries/${JOBNAME} \
+--gin_config=meta_dataset/learn/gin/default/${JOBNAME}.gin \
+--gin_bindings="Trainer.experiment_name='$JOBNAME'" \
+2>&1 | tee log_${JOBNAME}.log
